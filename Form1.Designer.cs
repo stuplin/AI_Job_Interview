7e27fb0dc5d2c014bf06730ccb44afec4ce3a181
stuplin <smtuppers@hotmail.com>
2016-02-25 21:51:40 +0000

    Added files via upload
    I have no idea what im doing..

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Job_Interview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.conversationBox = new System.Windows.Forms.RichTextBox();
+            this.responseBox = new System.Windows.Forms.RichTextBox();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // conversationBox
+            // 
+            this.conversationBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.conversationBox.Location = new System.Drawing.Point(12, 12);
+            this.conversationBox.Name = "conversationBox";
+            this.conversationBox.ReadOnly = true;
+            this.conversationBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.conversationBox.Size = new System.Drawing.Size(476, 325);
+            this.conversationBox.TabIndex = 0;
+            this.conversationBox.Text = "";
+            // 
+            // responseBox
+            // 
+            this.responseBox.BackColor = System.Drawing.Color.Khaki;
+            this.responseBox.Location = new System.Drawing.Point(12, 376);
+            this.responseBox.Name = "responseBox";
+            this.responseBox.Size = new System.Drawing.Size(476, 121);
+            this.responseBox.TabIndex = 1;
+            this.responseBox.Text = "";
+            // 
+            // answerButton
+            // 
+            this.answerButton.BackColor = System.Drawing.Color.DarkKhaki;
+            this.answerButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.answerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerButton.Location = new System.Drawing.Point(495, 376);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(142, 121);
+            this.answerButton.TabIndex = 2;
+            this.answerButton.Text = "Answer";
+            this.answerButton.UseVisualStyleBackColor = false;
+            this.answerButton.Click += new System.EventHandler(this.answerButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(495, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 230);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.answerButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(760, 507);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.answerButton);
+            this.Controls.Add(this.responseBox);
+            this.Controls.Add(this.conversationBox);
+            this.Name = "Form1";
+            this.Text = "Job Interview";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox conversationBox;
+        private System.Windows.Forms.RichTextBox responseBox;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
